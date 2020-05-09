83d22e8abe58253ad7b9702ef9c3e9ddeb86be71
CHristapherenese <64864560+CHristapherenese@users.noreply.github.com>
2020-05-08 20:04:18 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,29 @@
 let Temp = 0
-pause(1000)
-Temp = 1
-pause(1000)
-Temp = 2
-pause(1000)
-Temp = 3
-pause(1000)
+ht16k33.setAddress(HT16K33_I2C_ADDRESSES.ADD_0x70)
+ht16k33.setBlinkRate(0)
+ht16k33.setBrightness(0)
 forever(function () {
+    Temp = 0
+    console.logValue("Temp", Temp)
+    pause(1000)
+    Temp = 1
+    console.logValue("Temp", Temp)
+    pause(1000)
+    Temp = 2
+    console.logValue("Temp", Temp)
+    pause(1000)
+    Temp = 3
+    console.logValue("Temp", Temp)
+    pause(1000)
     Temp = pins.i2cReadNumber(
     24,
     NumberFormat.Int8LE,
     true
     )
-    console.logValue("Temp", pins.i2cReadNumber(
-    24,
+    pins.i2cWriteNumber(
+    113,
+    Temp,
     NumberFormat.Int8LE,
     true
-    ))
+    )
 })
